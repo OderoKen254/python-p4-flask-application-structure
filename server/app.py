9d0c484b124dea2ca23e97af0b13f2cf3e7a4496
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-14 18:02:19 +0300

    create and run Flask app

--- server/app.py
+++ server/app.py
@@ -3,3 +3,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# append to server/app.py
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# modify user() in server/app.py
+@app.route('/<string:about>')
+def about():
+    return '<h1>About this page</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
